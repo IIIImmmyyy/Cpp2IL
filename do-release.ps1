55a95a3aca198cf3a5af9ab2e5ae4c0245eccc0e
Imy <29538641@qq.com>
2026-06-08 22:21:02 +0800

    Update target framework to .NET 10.0
    This commit updates the target framework from .NET 9.0 to .NET 10.0 across multiple projects and scripts, ensuring consistency and compatibility with the latest .NET version. It also removes the net472 target and updates the paths and commands in the README and test files accordingly.

--- do-release.ps1
+++ do-release.ps1
@@ -99,31 +99,27 @@ Write-Host "Building Cpp2IL command line executable, release version $fullVersio
 
 Write-Host "    Building Cpp2IL - Windows x64, Standalone .NET"
 
-$null = dotnet publish -c Release -f "net9.0" -r "win-x64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
+$null = dotnet publish -c Release -f "net10.0" -r "win-x64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
 
 Write-Host "    Building Cpp2IL - Windows ARM64, Standalone .NET"
 
-$null = dotnet publish -c Release -f "net9.0" -r "win-arm64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
+$null = dotnet publish -c Release -f "net10.0" -r "win-arm64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
 
 Write-Host "    Building Cpp2IL - Linux x64, Standalone .NET"
 
-$null = dotnet publish -c Release -f "net9.0" -r "linux-x64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
+$null = dotnet publish -c Release -f "net10.0" -r "linux-x64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
 
 Write-Host "    Building Cpp2IL - Linux ARM64, Standalone .NET"
 
-$null = dotnet publish -c Release -f "net9.0" -r "linux-arm64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
+$null = dotnet publish -c Release -f "net10.0" -r "linux-arm64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
 
 Write-Host "    Building Cpp2IL - MacOS x64, Standalone .NET"
 
-$null = dotnet publish -c Release -f "net9.0" -r "osx-x64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
+$null = dotnet publish -c Release -f "net10.0" -r "osx-x64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
 
 Write-Host "    Building Cpp2IL - MacOS ARM64, Standalone .NET"
 
-$null = dotnet publish -c Release -f "net9.0" -r "osx-arm64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
-
-Write-Host "    Building Cpp2IL - Windows, .NET Framework"
-
-$null = dotnet publish -c Release -f "net472" -r "win-x64" /p:VersionSuffix=$version
+$null = dotnet publish -c Release -f "net10.0" -r "osx-arm64" /p:VersionSuffix=$version /p:PublishSingleFile=true --self-contained
 
 function CopyAndRename($rid, $platform, $releasePlatformString, $extension)
 {
@@ -157,14 +153,12 @@ function ZipAndRename($rid, $platform, $releasePlatformString, $extension)
 
 Write-Host "Moving files to artifacts directory"
 
-CopyAndRename "net9.0" "win-x64" "Windows" ".exe"
-CopyAndRename "net9.0" "win-arm64" "Windows-ARM64" ".exe"
-CopyAndRename "net9.0" "linux-x64" "Linux" ""
-CopyAndRename "net9.0" "linux-arm64" "Linux-ARM64" ""
-CopyAndRename "net9.0" "osx-x64" "OSX" ""
-CopyAndRename "net9.0" "osx-arm64" "OSX-ARM64" ""
-ZipAndRename "net472" "win-x64" "Windows-Netframework472" ".exe"
-
+CopyAndRename "net10.0" "win-x64" "Windows" ".exe"
+CopyAndRename "net10.0" "win-arm64" "Windows-ARM64" ".exe"
+CopyAndRename "net10.0" "linux-x64" "Linux" ""
+CopyAndRename "net10.0" "linux-arm64" "Linux-ARM64" ""
+CopyAndRename "net10.0" "osx-x64" "OSX" ""
+CopyAndRename "net10.0" "osx-arm64" "OSX-ARM64" ""
 Set-Location $ProjectRoot
 
 Write-Host "Building plugins..."
